589ae9a23deea1d3c62d96e549f806140551d6dd
Daniel M Sheehan <daniel.martin.sheehan@gmailcom>
2014-04-01 12:23:06 -0400

    update

--- tasks/201403_canvas_paper_edits/005-merge-census-data-to-input-data.py
+++ tasks/201403_canvas_paper_edits/005-merge-census-data-to-input-data.py
@@ -2,9 +2,6 @@ import pandas as pd
 
 dfnat = pd.read_csv('/Volumes/Echo/GIS/projects/streetview/tasks/201403_canvas_paper_edits/data/input/_original/NationalSample.csv')
 
-#dfpop = pd.read_csv('/Volumes/Echo/GIS/projects/streetview/tasks/201403_canvas_paper_edits/data/processing/streets_right_40_feet_blocks_2000_select_with_pop.csv', dtype={'FIPS': object})
-#dfpop = dfpop.drop('BLKIDFP00', axis=1)
-
 dfblo = pd.read_csv('/Volumes/Echo/GIS/projects/streetview/tasks/201403_canvas_paper_edits/data/misc/list_of_blocks.csv', dtype={'BLKIDFP00': object})
 
 dfcen = pd.read_csv('/Volumes/Echo/GIS/projects/streetview/tasks/201403_canvas_paper_edits/data/input/census/census_2010_block.csv', dtype={'geoid': object})
@@ -12,6 +9,6 @@ dfcen = pd.read_csv('/Volumes/Echo/GIS/projects/streetview/tasks/201403_canvas_p
 
 merged1 = dfblo.merge(dfcen, left_on='BLKIDFP00', right_on='geoid', how='left')
 
-merged2 = dfnat.merge(merged1, left_on='segid', right_on='SEGID', how='left')
+merged1 = merged1.drop(['XCoord','YCoord'], axis=1)
 
-merged2.to_csv('/Volumes/Echo/GIS/projects/streetview/tasks/201403_canvas_paper_edits/data/processing/streets_right_40_feet_blocks_2000_select_with_pop_census_data.csv', index=False)
+merged1.to_csv('/Volumes/Echo/GIS/projects/streetview/tasks/201403_canvas_paper_edits/data/processing/streets_right_40_feet_blocks_2000_select_with_pop_census_data.csv', index=False)
